c949f5425eb85bc7b4958749d572312dcd9ae3e2
RuthMery <ruth.cardenas.perez@gmail.com>
2017-04-26 11:40:51 -0500

    app-implementación de slide

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,17 @@
 window.addEventListener("load", function () {
+  // VARIABLES GLOBALES
+  var image_counter = 0;//Contador de imágenes
+  var slide_titles = ["LA MEJOR RECETA PARA TU EVENTO",
+                      "COCINAMOS CON AMOR",
+                      "SIMPLEMENTE LOS MEJORES CHEF",
+                      "LA MEJOR ALTERNATIVA GASTRONÓMICA"
+                      ];
+  var slide_subtitles = ["LA ALTA COCINA, LA PUEDE DISFRUTAR CUALQUIERA..",
+                         "EL MEJOR SERVICIO, PORQUE NOS ENCANTA VERTE SONREIR",
+                         "EL MEJOR SABOR, UN GRAN PRECIO.",
+                         "UN BUEN SABOR NO SIGNIFICA SIEMPRE QUE SEA CARO"
+                      ];
+
   // FUNCIONES
   function createElement (parent, elementType, classList, optionalAttribute) {
     var element = document.createElement(elementType);
@@ -25,6 +38,33 @@ window.addEventListener("load", function () {
     createElement (slide_div[0], "img", ["slide-image", "image-hidden"], "assets/img/home-3.jpg");
     createElement (slide_div[0], "img", ["slide-image", "image-hidden"], "assets/img/home-4.jpg");
   }
+  function administerClasses (element, classToRemove, classToAdd) {
+    element.classList.remove(classToRemove);
+    element.classList.add(classToAdd);
+  }
+  function runSlide () {
+    var slide_image = document.getElementsByClassName("slide-image");//Imágenes del slide
+    var slide_texts = document.getElementsByClassName("text");//Texto del Slide
+    //EVENTOS
+    var change_slide_text = new Event("changeSlideText");
+    slide_texts[0].addEventListener("changeSlideText", function (e) {
+      this.innerHTML = slide_titles[image_counter];
+    }, false);
+
+    slide_texts[1].addEventListener("changeSlideText", function (e) {
+      this.innerHTML = slide_subtitles[image_counter];
+    }, false);
+
+    // Ocultando la imagen actual
+    administerClasses (slide_image[image_counter], "image-show", "image-hidden");
+    //Aumentando el contador de imágenes
+    image_counter = (image_counter + 1) % 4;
+    //Mostrando la siguiente imagen
+    administerClasses (slide_image[image_counter], "image-hidden", "image-show");
+    slide_texts[0].dispatchEvent(change_slide_text);
+    slide_texts[1].dispatchEvent(change_slide_text);
+  }
   // LLAMANDO FUNCIONES
   printSlide();
+  setInterval(runSlide,5000);
 });
